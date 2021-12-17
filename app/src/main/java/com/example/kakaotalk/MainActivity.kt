1114f363f0c84f1742b16417e6d6899ea880a0f1
JaeHyeon <jaehyeon0825@gmail.com>
2021-12-17 23:44:49 +0700

    바텀네비게이션뷰 구현

--- app/src/main/java/com/example/kakaotalk/MainActivity.kt
+++ app/src/main/java/com/example/kakaotalk/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.kakaotalk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var personFragment: PersonFragment
+    private lateinit var talkFragment: TalkFragment
+    private lateinit var searchFragment: SearchFragment
+    private lateinit var omenuFragment: OmenuFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_nav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        val personFragment = PersonFragment.newInstance()
+        supportFragmentManager.beginTransaction().add(R.id.fragments_frame, personFragment).commit()
+
+
+        btn_nav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_person -> {
+                    val personFragment = PersonFragment.newInstance()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, personFragment).commit()
+                }
+                R.id.menu_talk -> {
+                    val talkFragment = TalkFragment.newInstance()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, talkFragment).commit()
+                }
+                R.id.menu_search -> {
+                    val searchFragment = SearchFragment.newInstance()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, searchFragment).commit()
+                }
+                R.id.menu_omenu -> {
+                    val omenuFragment = OmenuFragment.newInstance()
+                    supportFragmentManager.beginTransaction().replace(R.id.fragments_frame, omenuFragment).commit()
+                }
+
+            }
+            return@setOnItemSelectedListener true
+        }
     }
+
 }
+
+
