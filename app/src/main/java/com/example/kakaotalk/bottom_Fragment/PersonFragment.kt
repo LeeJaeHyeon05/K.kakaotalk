8df4ee396a4730e05e98c6afdbddecc9410e2fd9
JaeHyeon <jaehyeon0825@gmail.com>
2021-12-24 22:18:45 +0700

    채널 페이지

--- app/src/main/java/com/example/kakaotalk/bottom_Fragment/PersonFragment.kt
+++ app/src/main/java/com/example/kakaotalk/bottom_Fragment/PersonFragment.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.view.size
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kakaotalk.ChannelPage
